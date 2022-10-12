03561d769c083c6d703df44c59d1d0073297a03d
subhadra04 <115663970+subhadra04@users.noreply.github.com>
2022-10-13 01:34:52 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,16 @@
+// importing the File class
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("JavaFile.java");
+
+    try {
+
+      // create a new file with name specified
+   
+    }
+  }
+}
